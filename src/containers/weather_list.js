20b0df866a4984abb0cc950d9586b6fd5bd1baa8
Johnson <scott.johnson@dsiglobal.com>
2018-09-14 16:01:03 -0500

    Added google maps code for components

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,23 +4,17 @@ import Chart from '../components/chart';
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
-		function toFahrenheit(kelvinTemp) {
-			return kelvinTemp * (9/5) - 459.67;
-		}
 		const name = cityData.city.name;
-		const temperatures = cityData.list.map(weather => toFahrenheit(weather.main.temp));
+		const temperatures = cityData.list.map(weather => weather.main.temp);
 		const pressures = cityData.list.map(weather => weather.main.pressure);
 		const humidities = cityData.list.map(weather => weather.main.humidity);
-		console.log(temperatures.length);
-		console.log(temperatures.length);
-		console.log(temperatures.length);
-				
+
 		return (
 			<tr key={name}>
 				<td>{name}</td>
-				<td><Chart data={temperatures} color="orange" /></td>
-				<td><Chart data={pressures} color="green" /></td>
-				<td><Chart data={humidities} color="black" /></td>
+				<td><Chart data={temperatures} color="orange" units="K"/></td>
+				<td><Chart data={pressures} color="green" units="hPa"/></td>
+				<td><Chart data={humidities} color="black" units="%"/></td>
 				
 			</tr>
 		);
@@ -32,9 +26,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (F)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
